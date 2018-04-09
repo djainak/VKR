de1fa39a802153db8d90f3212fee704ac38fd293
Irina Kurzenko <irina@kurzenko.ru>
2018-04-09 21:50:11 +0300

    Логика меню и значок

--- VKR/Controllers/HomeController.cs
+++ VKR/Controllers/HomeController.cs
@@ -21,6 +21,14 @@ namespace VKR.Controllers
     public class AdminController : Controller
     {
         [HttpGet]
+        public ActionResult Home()
+        {
+            ViewBag.Title = "О точке";
+
+            return View();
+        }
+
+        [HttpGet]
         public ActionResult MenuList()
         {
             using (var db = new Contexts())
@@ -32,6 +40,37 @@ namespace VKR.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult AddNewMenu()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddMenu()
+        {
+            //Считываем данные из формы
+            string name = HttpContext.Request.Form["Name"];
+            string description = HttpContext.Request.Form["Description"];
+
+            //Создаем объект Меню и заполняем данными из формы
+            Menu menu = new Menu();
+            menu.Name = name;
+            menu.Description = description;
+
+            //Добавляем новое меню в БД
+            using (var db = new Contexts())
+            {
+                db.Menues.Add(menu);
+                db.SaveChanges();
+            }
+            return Redirect("./AddMenuItemForm?menuId=" + menu.Id);
+        }
+
+        /// <summary>
+        /// Контроллер, обрабатывающий страницу с формой для добавления нового блюда в меню.
+        /// </summary>
+        /// <returns>Страницу с формой добавления пункта меню.</returns>
         [HttpGet]
         public ActionResult ListMenuItems()
         {
@@ -68,17 +107,44 @@ namespace VKR.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Реализация удаления пункта меню из БД
+        /// </summary>
+        /// <returns>Страничка без удаленного пункта меню</returns>
         [HttpPost]
         public ActionResult RemoveMenuItem()
         {
             int id = Convert.ToInt32(HttpContext.Request.Params["id"]);
             int m_id = Convert.ToInt32(HttpContext.Request.Params["MenuId"]);
+            using (var db = new Contexts())
+            {
+                MenuItem menuItem = db.MenuItems.Find(id);
+                db.MenuItems.Remove(menuItem);
+                db.SaveChanges();
+            }
 
             return Redirect("./AddMenuItemForm?menuId=" + m_id);
         }
 
+        /// <summary>
+        /// Реализация удаления меню из БД
+        /// </summary>
+        /// <returns>Страничка без удаленного меню</returns>
         [HttpPost]
-        public ActionResult Admin()
+        public ActionResult RemoveMenu()
+        {
+            int id = Convert.ToInt32(HttpContext.Request.Params["id"]);
+            using (var db = new Contexts())
+            {
+                Menu menu = db.Menues.Find(id);
+                db.Menues.Remove(menu);
+                db.SaveChanges();
+            }
+            return Redirect("./MenuList");
+        }
+
+        [HttpPost]
+        public ActionResult NewMenuItem()
         {
             //Считываем данные из формы
             int menuId = Convert.ToInt32(HttpContext.Request.Form["MenuId"]);
