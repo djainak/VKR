1616054967d6f3c2a418a889058a21dc8fe356e4
Irina Kurzenko <irina@kurzenko.ru>
2018-04-15 23:47:39 +0300

    Авторизация 1.0
    Без проверки на совпадения

--- VKR/Controllers/HomeController.cs
+++ VKR/Controllers/HomeController.cs
@@ -20,6 +20,48 @@ namespace VKR.Controllers
     }
 
     /// <summary>
+    /// Класс регистрации нового пользователя
+    /// </summary>
+    public class RegistrationController:Controller
+    {
+        /// <summary>
+        /// Регистрация пользователя
+        /// </summary>
+        /// <returns>Форма регистрации пользователя</returns>
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Обработка формы регистрации нового пользователя
+        /// </summary>
+        /// <returns>Переадресация на личный кабинет (!!!ПОКА НА АДМИНКУ)</returns>
+        [HttpPost]
+        public ActionResult NewUser()
+        {
+            User user = new User();
+            user.Login = HttpContext.Request.Form["Login"];
+            user.Password = HttpContext.Request.Form["Password"];
+            user.Email = HttpContext.Request.Form["Email"];
+            user.FirstName = HttpContext.Request.Form["FirstName"];
+            user.Name = HttpContext.Request.Form["Name"];
+            user.Patronymic = HttpContext.Request.Form["Patronymic"];
+            user.PhoneNumber = HttpContext.Request.Form["PhoneNumber"];
+            user.Status = 0;
+
+            using (var db = new Contexts())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+            }
+                //В дальнейшем сделать переадресацию на личный кабинет, сейчас переадресация в админку
+                return Redirect("../Admin/Home?UserId=" + user.UserID);
+        }
+    }
+
+    /// <summary>
     /// Класс авторизации
     /// </summary>
     public class AuthorizationController : Controller
