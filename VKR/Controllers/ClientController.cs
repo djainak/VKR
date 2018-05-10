2d4b8da973ec0c6753efd87704893c039d273741
Irina Kurzenko <irina@kurzenko.ru>
2018-05-10 18:36:48 +0300

    Выгрузка времени на страницу

--- VKR/Controllers/ClientController.cs
+++ VKR/Controllers/ClientController.cs
@@ -39,21 +39,45 @@ namespace VKR.Controllers
         {
             int allprice = 0;
             List<Cart> cart = new List<Cart>();
+            List<string> times = new List<string>();
             int id_user = Convert.ToInt32(HttpContext.Request.Cookies["user_token"].Value);
+            
+            Dictionary<string, string> dayName = new Dictionary<string, string>(7);
+            dayName.Add("Monday", "Понедельник");
+            dayName.Add("Tuesday", "Вторник");
+            dayName.Add("Wednesday", "Среда");
+            dayName.Add("Thursday", "Четверг");
+            dayName.Add("Friday", "Пятница");
+            dayName.Add("Saturday", "Суббота");
+            dayName.Add("Sunday", "Воскресенье");
+
+            string Today = dayName[DateTime.Now.DayOfWeek.ToString()];
+
             using (var db = new Contexts())
             {
                 cart = db.Cart.Where(c => c.UserId == id_user).ToList();
+
+                //Выгружаем товары
                 foreach (var c in cart)
                 {
                     c.Product = db.MenuItems.Where(cc => cc.Id == c.MenuItemId).FirstOrDefault();
                     c.Product.Category = db.CategoryMenuItem.Where(cc => cc.CategoryMenuItemID == c.Product.CategoryMenuItemId).FirstOrDefault();
                     allprice = allprice + c.Product.Price * c.Amount;
                 }
+
+                //Выгружаем доступное время
+                List<FreeTime> tmp = new List<FreeTime>();
+                tmp = db.FreeTime.Where(t => t.DayWork.Name == Today && t.cur_amount < t.max_amount).ToList();
+                foreach(var t in tmp)
+                {
+                    times.Add(t.Time);
+                }
             }
             if (cart.Count == 0)
                 ViewBag.Empty = "true";
             ViewBag.Cart = cart;
             ViewBag.AllPrice = allprice;
+            ViewBag.Times = times;
             return View();
         }
 
