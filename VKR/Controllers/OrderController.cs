f567cfe609e873d1418bb48636ed199dd3d9b332
Irina Kurzenko <irina@kurzenko.ru>
2018-05-16 22:24:25 +0300

    Админка: налажено изменение статуса у заказов

---
+++ VKR/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VKR.Models;
+
+namespace VKR.Controllers
+{
+    /// <summary>
+    /// Контроллер, обрабатывающий действия с заказами в администраторской части приложения
+    /// </summary>
+    public class OrderController : ApiController
+    {
+        /// <summary>
+        /// Метод, обрабатывающий изменение статуса заказа
+        /// </summary>
+        /// <param name="status">Новый статус</param>
+        /// <param name="order_id">Уникальный идентификатор заказа</param>
+        public void Post(int status, int order_id)
+        {
+            using (var db = new Contexts())
+            {
+                db.Orders.Find(order_id).Status = status;
+                db.SaveChanges();
+            }
+        }
+    }
+}
